fbf84d1fce75924c1b97526d7c73b82a98335686
Mahaboob Basha <30779356+bashamsc@users.noreply.github.com>
2025-01-18 11:54:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request,render_template,url_for
+import pickle
+
+app=Flask(__name__)
+
+model = pickle.load(open('rfc_aqi.pkl', 'rb'))
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	AQI_predict = None
+	if request.method == 'POST':
+		# field1 = request.form['field1']
+		AQI_predict=model.predict([[
+			request.form["PM2.5"],
+			request.form["PM10"],
+			request.form["NO2"],
+			request.form["SO2"],
+			request.form["CO"]
+			
+		]])    
+	
+	return render_template('result.html', prediction=AQI_predict)
+
+if __name__=='__main__':
+	app.run()
